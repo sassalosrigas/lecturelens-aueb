9d5f1bb4ccabec57e53e0ddaf74a39f7847a7e2e
rigas <sassalosrigas@gmail.com>
2026-05-14 19:59:23 +0300

    added more frames and fixed up drawables (icons)

---
+++ app/src/main/java/gr/aueb/lecturelens/ProfessorDetailsActivity.java
@@ -0,0 +1,56 @@
+package gr.aueb.lecturelens;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfessorDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_professor_details);
+
+        TextView btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        ImageView navHome = findViewById(R.id.navHome);
+        navHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfessorDetailsActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        ImageView navSearch = findViewById(R.id.navSearch);
+        navSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfessorDetailsActivity.this, SearchActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        ImageView navProfile = findViewById(R.id.navProfile);
+        navProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfessorDetailsActivity.this, ProfileActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
