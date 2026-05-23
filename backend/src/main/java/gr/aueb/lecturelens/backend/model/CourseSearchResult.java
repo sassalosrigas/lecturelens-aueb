d9738cc7f87d16f8027c1978f45f16f18f907922
ThodTsi <teotsi4002@gmail.com>
2026-05-23 17:20:20 +0300

    course search result file added

---
+++ backend/src/main/java/gr/aueb/lecturelens/backend/model/CourseSearchResult.java
@@ -0,0 +1,20 @@
+package gr.aueb.lecturelens.backend.model;
+
+import java.util.List;
+
+public class CourseSearchResult {
+    private Course course;
+    private List<Professor> professors;
+
+    public CourseSearchResult(Course course, List<Professor> professors) {
+        this.course = course;
+        this.professors = professors;
+    }
+
+    // Getters and Setters
+    public Course getCourse() { return course; }
+    public void setCourse(Course course) { this.course = course; }
+
+    public List<Professor> getProfessors() { return professors; }
+    public void setProfessors(List<Professor> professors) { this.professors = professors; }
+}
